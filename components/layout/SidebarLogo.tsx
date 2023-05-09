06ae87a75c319db6d36929098153af7d55879808
GMWebDevelopment <gmwebdevelopments@gmail.com>
2023-05-09 09:59:46 +0200

    🎉 : Adding Layout components + Sidebar

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export const SidebarLogo = () => {
+  // Router
+  const router = useRouter();
+
+  return (
+    <div
+      className="rounded-full h-14 w-14  items-center justify-center hover:bg-blue-300 hover:bg-opacity-10  cursor-pointer transition"
+      onClick={() => router.push("/")}
+    >
+      <Image
+        src="/logo.png"
+        alt="Jesus lave les pied"
+        width={320}
+        height={320}
+      />
+    </div>
+  );
+};
